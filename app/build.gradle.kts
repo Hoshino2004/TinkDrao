c43c6f5b86883fcc5855572fb2a0ee5d646b73ca
Nguyen Thinh <thinh631631@gmail.com>
2025-03-11 13:12:57 +0700

    New commit 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,40 @@ dependencies {
     implementation ("com.google.firebase:firebase-database:20.2.2")
     implementation ("androidx.viewpager2:viewpager2:1.0.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation("com.google.firebase:firebase-storage:21.0.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.activity:activity:1.9.2")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
+
+
+    implementation ("com.firebaseui:firebase-ui-database:8.0.2")
+    implementation ("com.squareup.picasso:picasso:2.71828")
+    implementation("com.github.denzcoskun:ImageSlideshow:0.1.2")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    implementation ("com.google.code.gson:gson:2.10.1")
+
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
+
+    implementation ("com.android.volley:volley:1.2.1")
+    implementation ("com.google.firebase:firebase-messaging:23.3.1")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation ("com.google.auth:google-auth-library-oauth2-http:1.15.0")
+
+    implementation ("com.google.android.material:material:1.9.0")
+
+    implementation ("com.google.android.gms:play-services-maps:18.2.0")
+    implementation ("com.google.android.gms:play-services-location:15.0.1")
+    implementation ("com.google.android.gms:play-services-safetynet:18.0.1")
 }
